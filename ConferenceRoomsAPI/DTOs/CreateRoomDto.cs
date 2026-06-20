5ff968bf0b44b790120b986884d304e127dcb3fb
Dmutro <dtyslyak@gmail.com>
2026-06-20 16:27:10 +0300

    Add CreateRoomDto DTO

---
+++ ConferenceRoomsAPI/DTOs/CreateRoomDto.cs
@@ -0,0 +1,13 @@
+﻿namespace ConferenceRoomsAPI.DTOs
+{
+    public class CreateRoomDto
+    {
+        public string Name { get; set; }
+
+        public int Capacity { get; set; }
+
+        public decimal Price { get; set; }
+
+        public List<int> ServiceIds { get; set; }
+    }
+}
