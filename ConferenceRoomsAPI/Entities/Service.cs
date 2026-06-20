58734079e3bb80cded25a8afc3770042352c8438
Dmutro <dtyslyak@gmail.com>
2026-06-20 16:19:19 +0300

    Add Service entity

---
+++ ConferenceRoomsAPI/Entities/Service.cs
@@ -0,0 +1,11 @@
+﻿namespace ConferenceRoomsAPI.Entities
+{
+    public class Service
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
