f7f90b01e7b165293c198797542933ae362673d4
Dmutro <dtyslyak@gmail.com>
2026-06-20 18:20:42 +0300

    Add TimeSlotType enum

---
+++ ConferenceRoomsAPI/Domain/Enums/TimeSlotType.cs
@@ -0,0 +1,17 @@
+﻿namespace ConferenceRoomsAPI.Domain.Enums
+{
+    public enum TimeSlotType
+    {
+        // 06:00 – 09:00 = -10% discount
+        Morning,
+
+        // 09:00 – 12:00 and 14:00 – 18:00 = base rate
+        Standard,
+
+        // 12:00 – 14:00 → +15% surcharge
+        Peak,
+
+        // 18:00 – 23:00 = -20% discount
+        Evening
+    }
+}
