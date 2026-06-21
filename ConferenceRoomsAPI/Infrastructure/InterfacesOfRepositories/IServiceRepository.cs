8fe53d945a387177fc6d93309d5e373f8a8e2b15
Dmutro <dtyslyak@gmail.com>
2026-06-22 00:38:01 +0300

    Add IServiceRepository interface

---
+++ ConferenceRoomsAPI/Infrastructure/InterfacesOfRepositories/IServiceRepository.cs
@@ -0,0 +1,14 @@
+﻿using ConferenceRoomsAPI.Domain.Entities;
+
+namespace ConferenceRoomsAPI.Infrastructure.InterfacesOfRepositories
+{
+    public interface IServiceRepository
+    {
+        Task<Service?> GetByIdAsync(Guid Id);
+        Task<IEnumerable<Service>> GetAllAsync();
+        Task<IEnumerable<Service>> GetByIdsAsync(IEnumerable<Guid> Ids);
+        Task AddAsync(Service Service);
+        Task UpdateAsync(Service Service);
+        Task DeleteAsync(Guid Id);
+    }
+}
