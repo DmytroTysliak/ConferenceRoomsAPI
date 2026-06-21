3f3ce96eae80e9e136cdd8b8661b735a6e4e9e31
Dmutro <dtyslyak@gmail.com>
2026-06-21 17:58:55 +0300

    Add ServiceDto dto

---
+++ ConferenceRoomsAPI/Domain/DTOs/ServiceDto.cs
@@ -0,0 +1,11 @@
+﻿namespace ConferenceRoomsAPI.Domain.DTOs
+{
+    public class ServiceDto
+    {
+        public record CreateServiceRequest(string Name, decimal Price);
+
+        public record UpdateServiceRequest(string? Name, decimal? Price);
+
+        public record ServiceResponse(Guid Id, string Name, decimal Price);
+    }
+}
