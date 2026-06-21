6dfd888b664d8cd7f92cbd463233e5740f258245
Dmutro <dtyslyak@gmail.com>
2026-06-22 00:34:18 +0300

    Add IBookingRepository interface

---
+++ ConferenceRoomsAPI/Infrastructure/InterfacesOfRepositories/IBookingRepository.cs
@@ -0,0 +1,18 @@
+﻿using ConferenceRoomsAPI.Domain.Entities;
+
+namespace ConferenceRoomsAPI.Infrastructure.InterfacesOfRepositories
+{
+    public interface IBookingRepository
+    {
+        Task<Booking?> GetByIdAsync(Guid Id);
+
+        // Includes Room + BookingServices -> Service
+        Task<Booking?> GetByIdWithDetailsAsync(Guid Id);
+        Task<IEnumerable<Booking>> GetAllAsync();
+        Task<IEnumerable<Booking>> GetByRoomIdAsync(Guid RoomId);
+        Task<IEnumerable<Booking>> GetByDateRangeAsync(DateTime From, DateTime To);
+        Task<bool> HasOverlappingBookingAsync(Guid RoomId, DateTime Start, DateTime End);
+        Task AddAsync(Booking Booking);
+        Task UpdateAsync(Booking Booking);
+    }
+}
