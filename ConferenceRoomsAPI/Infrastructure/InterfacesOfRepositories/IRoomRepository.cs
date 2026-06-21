11ec78c381cce1dc1d03dd01a4fcd9061d02a043
Dmutro <dtyslyak@gmail.com>
2026-06-22 00:25:01 +0300

    Add IRoomRepository interface

---
+++ ConferenceRoomsAPI/Infrastructure/InterfacesOfRepositories/IRoomRepository.cs
@@ -0,0 +1,18 @@
+﻿using ConferenceRoomsAPI.Domain.Entities;
+
+namespace ConferenceRoomsAPI.Infrastructure.InterfacesOfRepositories
+{
+    public interface IRoomRepository
+    {
+        Task<Room?> GetByIdAsync(Guid Id);
+        
+        // Includes RoomServices -> Service
+        Task<Room?> GetByIdWithServicesAsync(Guid Id);
+        Task<IEnumerable<Room>> GetAllAsync();
+        Task<IEnumerable<Room>> GetAvailableRoomAsync(DateTime Start, DateTime End, int minCapacity);
+        Task AddAsync(Room Room);
+        Task UpdateAsync(Room Room);
+        Task DeleteAsync(Guid Id);
+
+    }
+}
