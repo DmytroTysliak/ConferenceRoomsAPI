0310fd4a573e97fa9c113fcbc8adeefebe5e3230
Dmutro <dtyslyak@gmail.com>
2026-06-21 18:55:22 +0300

    Add ReportDto dto

---
+++ ConferenceRoomsAPI/Domain/DTOs/ReportDto.cs
@@ -0,0 +1,17 @@
+﻿namespace ConferenceRoomsAPI.Domain.DTOs
+{
+    public class ReportDto
+    {
+        public record RevenueReportResponse(DateTime From, DateTime To, List<RoomRevenueItem> Rooms, decimal GrandTotal);
+
+        public record RoomRevenueItem(Guid RoomId, string RoomName, int TotalBookings, decimal TotalRevenue);
+
+        public record RoomOccupacyResponse(DateTime From, DateTime To, List<RoomRevenueItem> Rooms);
+
+        public record RoomOccupacyItem(Guid RoomId, string RoomName, double BookedHours, double AvalibleHours, double OccupacyPercent);
+
+        public record PopularServicesReportResponse(List<ServicePopularItem> Srvices);
+
+        public record ServicePopularItem(Guid ServiceId, string ServiceName, int TimesOrdered, decimal TotalRevenue);
+    }
+}
