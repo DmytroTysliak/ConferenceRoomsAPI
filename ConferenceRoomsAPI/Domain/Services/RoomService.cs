f86e8afd8fe8ed6a0577cecd61023f57a04d8df4
Dmutro <dtyslyak@gmail.com>
2026-06-21 18:39:23 +0300

    Add RoomService service

---
+++ ConferenceRoomsAPI/Domain/Services/RoomService.cs
@@ -0,0 +1,14 @@
+﻿using ConferenceRoomsAPI.Domain.Entities;
+
+namespace ConferenceRoomsAPI.Domain.Services
+{
+    public class RoomService
+    {
+        public Guid RoomId { get; set; }
+        public Room Room { get; set; } = null!;
+
+        public Guid ServiceId { get; set; }
+
+        public Service Service { get; set; } = null!;
+    }
+}
